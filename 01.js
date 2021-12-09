44892321f50e038282e5bdbf0ef06f6f39086c50
Maks-KaNDeR <makskander1@gmail.com>
2021-12-09 12:05:15 +0300

    Codewars 01-03

---
+++ 01.js
@@ -0,0 +1,18 @@
+// 1
+// repeat() Returns a new string with a specified number of copies of an existing string
+
+function repeatStr (n, s) {
+  return s.repeat(n);
+}
+
+// 2 Multiply
+
+function multiply(a, b){
+  return a * b
+}
+
+// 3 Write a function "greet" that returns "hello world!"
+
+const greet = () => {
+  return "hello world!"
+}

6f85b632abc991339d0f6b348cb3a41996554fb4
Maks-KaNDeR <makskander1@gmail.com>
2021-12-11 12:45:31 +0300

    6-8

---
+++ 06-10.js
@@ -0,0 +1,22 @@
+// 6
+function doubleChar(str) {
+  var newString = "";
+  for( i = 0; i < str.length; i++) {
+    newString += str[i] + str[i];
+  }
+  return newString;
+}
+
+// 7
+function testit(a,b){
+
+  return a | b;
+}
+
+// 8 
+function switchItUp(number){
+    var words = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine'];
+  return words[number]; 
+}
+
+// 9 
